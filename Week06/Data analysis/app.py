e717a51715e97b5e62153a66d65f7f52815992a9
Hallan-miranda <hallanlmm@gmail.com>
2024-07-25 20:44:05 -0300

    update life-expectancy app

--- Week06/Data analysis/app.py
+++ Week06/Data analysis/app.py
@@ -2,48 +2,48 @@ import csv
 
 
 
-with open("life-expectancy.csv") as data:
+with open("c:\\Users\\Hallan\\OneDrive\\Área de Trabalho\\Introdução em programação\\Week06\\Data analysis\\life-expectancy.csv") as data:
     life_expectancy = csv.reader(data, delimiter=',')
 
     country_year = 0
     country_year_amount = 0
-    overall_max = 0
-    overall_min = 0
+    overall_max = ["", "", 0, 0]
+    overall_min = ["", "", 0, 9999]
     average = 0
-    year_max = [0, 0, 0, 0]
-    year_min = [0, 0, 0, 0]
+    year_max = ["", "", 0, 0]
+    year_min = ["", "", 0, 0]
 
     year_guest = int(input("Enter the year of interest: "))
     for country_life_expectancy in life_expectancy:
-        print(country_life_expectancy)
 
         entity = str(country_life_expectancy[0])
         code = str(country_life_expectancy[1])
         year = int(country_life_expectancy[2])
         expectancy = float(country_life_expectancy[3])
 
-        if expectancy > overall_min:
-            overal_min = country_life_expectancy
+        if expectancy < overall_min[3]:
+            overall_min = [entity, code, year, expectancy]
 
-        if expectancy < overall_max:
-            overal_max = country_life_expectancy
+        if expectancy > overall_max[3]:
+            overall_max = [entity, code, year, expectancy]
         
 
-        if expectancy > year_min[3] and year == year_guest:
-            year_min = country_life_expectancy
-    
-        if expectancy < year_max[3] and year == year_guest:
-            year_max = country_life_expectancy
-
-
         if year == year_guest:
+            if expectancy > year_min[3]:
+                year_min = [entity, code, year, expectancy]
+    
+            if expectancy < year_max[3]:
+                year_max = [entity, code, year, expectancy]
+
+
+       
             country_year_amount = country_year_amount + 1
             country_year = country_year + expectancy
 
-    average = country_year / country_year_amount
+    average = round(country_year / country_year_amount, 2)
 
-    print(f"\nThe overall max life expectancy is: {overal_max[3]} from {overal_max[0]} in {overal_max[2]} ")
-    print(f"The overall max life expectancy is: {overal_min[3]} from {overal_min[0]} in {overal_min[2]}\n")
+    print(f"\nThe overall max life expectancy is: {overall_max[3]} from {overall_max[0]} in {overall_max[2]} ")
+    print(f"The overall min life expectancy is: {overall_min[3]} from {overall_min[0]} in {overall_min[2]}\n")
 
 
     print(f"For the year {year_guest}:")
